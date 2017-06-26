a5d5e9e24f5f5e079dc4e05a263a804cb11b4c35
ViktorGlazkov <YWgaMaZpf2>
2017-06-26 23:35:26 +0300

    first commit

---
+++ Controllers/AppController.cs
@@ -0,0 +1,24 @@
+
+
+namespace webapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
